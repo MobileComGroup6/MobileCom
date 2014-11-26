ea7144583298474e4d10989a1dc150a2387de331
scheuing <scheuing@students.unibe.ch>
2014-11-26 18:25:41 +0100

    Implemented output of test results as figure. Could not test different noise levels, cuz does not seem to make any difference. code is very messy and im not 100% sure it does what it should

--- matlab_code/startTests.m
+++ matlab_code/startTests.m
@@ -1,20 +1,32 @@
+%How to use:
+%The following configuration s are allowed:
+% More than one frequnecy OR more then one power or more than one sender
+
+%%
+clear all
+close all
+clc
+
+%%
 % setup
 
 loadRandom = false;
 saveRandom= true;
 
 %define some paramters for all tests
-dataRate= 8;
-dataLength = 16;
-chippingRate = [64,128,256];
-chipLength = [8,16,32];
-repetitions = 2;
-maxNumberOfSenders = 2
+dataRate= 10;
+dataLength = 10;
+chippingRate = [10];
+chipLength = [10];%,100,1000];
+repetitions = 30;
+maxNumberOfSenders = 200;
 %load or save random numbers, so the same numbers can be used for all
 %tests
 if loadRandom
    load('randInt.mat', 'randomNumbers')
 else
+    %the factor 32 is just to make sure we have enough test data even is
+    %someone usese more than maxNumberOfSenders senders
     numRandInt = repetitions*length(chippingRate)*length(chipLength)*maxNumberOfSenders*32;
     randomNumbers = randi([0,1],dataLength,numRandInt);
     if saveRandom
@@ -23,9 +35,15 @@ else
 end   
 
 % for each testcase run testExe
+% testExe(  mode,   dataRate,   chippingRates,  chipLengths,    numberOfSenders,    jammingMode,    freqs,   powers,	randomNumbers,  repetitions)
 
 %DSSS Tests
+%test-case d1: DSSS with wideband noise
+% testExe(    'dsss', dataRate,   chippingRate,   chipLength,     1,                  'wideband',     100,    [1:20],   randomNumbers,  repetitions);
+%test-case d2: DSSS with multiple users
+% testExe(    'dsss', dataRate,   chippingRate,   chipLength,     maxNumberOfSenders,                  'wideband',     100,    [1],   randomNumbers,  repetitions);
 
-%test-case 1: DSSS
-% testExe(mode, dataRates, chippingRates, chipLengths, numberOfSenders, jammingMode, freqs, powers, randomNumbers, repetitions)
-testExe('dsss', dataRate, chippingRate, chipLength, 1, 'wideband',100, [1,2,3], randomNumbers, repetitions);
+
+%FHSS tests
+%test-case f1
+testExe(    'fhss', dataRate,   chippingRate,   chipLength,     1,                  'narrowband',   [100:5:150],  1,      randomNumbers,  repetitions);
--- matlab_code/testExe.m
+++ matlab_code/testExe.m
@@ -1,60 +1,108 @@
 function testExe(mode, dataRate, chippingRates, chipLengths, numberOfSenders, jammingMode, freqs, powers, randomNumbers, repetitions)
 %% setup
-samplesPerSecond = 1024;
+samplesPerSecond = 10000;
 medium = Medium;
 
 %create power array with frequency and power
 %only the FHSS, narrow-band test uses more than one frequency
 if strcmp(mode,'fhss') && strcmp(jammingMode, 'narrowband')
-    jammingParaLength = length(frequs);
+    jammingParaLength = length(freqs);
 else
     jammingParaLength = length(powers);
 end    
 
 %% test
-%repeat for averaging
-subTestIter = 1;
-testIter = 1;
-for i = 1:repetitions 
-   for chippingRate = chippingRates
-      for chipLength = chipLengths
+%counter to access the right column of the randomNumbers
+msgsSent = 0;
+%counter that is increased each time a test configuration with all
+%repetitions has ran
+configRan = 0;
+
+for chippingRate = chippingRates
+	for chipLength = chipLengths
         for jammingPara = 1:jammingParaLength
-            %setup entities
-            pnGenerator = PNGenerator(chipLength);
-            %the receiver and the FIRST sender get the same chip sequence
-            sequence = pnGenerator.step();
+            testRepetition = 0;
+            for totalSenders = 1: numberOfSenders
+                for i = 1:repetitions 
+                    %setup entities
+                    pnGenerator = PNGenerator(chipLength);
+                    %the receiver and the FIRST sender get the same chip sequence
+                    sequence = pnGenerator.step();
 
-            receiver = Receiver(medium, sequence, mode, samplesPerSecond, dataRate,chippingRate);
-            jammer = Jammer(medium, samplesPerSecond, jammingMode);
+                    receiver = Receiver(medium, sequence, mode, samplesPerSecond, dataRate,chippingRate);
+                    jammer = Jammer(medium, samplesPerSecond, jammingMode);
 
 
-            %create senders
-            for senderNumber = numberOfSenders
-                senders(senderNumber) = Sender(medium, sequence, mode, samplesPerSecond, dataRate,chippingRate);
-                sequence = pnGenerator.step();
+                    %create senders
+                    for senderNumber = 1:totalSenders
+                        senders(senderNumber) = Sender(medium, sequence, mode, samplesPerSecond, dataRate,chippingRate);
+                        sequence = pnGenerator.step();
+                    end
+
+                    %save data to compare with received data
+                    sentData(:,testRepetition+1) = randomNumbers(:,msgsSent+1);
+                    msgsSentIntermediate = msgsSent;
+                    %let all senders send  
+                    for sender = senders
+                        sender.send(randomNumbers(:,msgsSent+1));
+                        msgsSent = msgsSent+1;
+                    end
+                    %Each iteration adds another user to the transmission,
+                    %but the users should send the same data in each
+                    %iteration. That's why i reset the counter.
+                    msgsSent = msgsSentIntermediate;
+                    
+                    %jam on different frequencies or with different power
+                    if strcmp(mode,'fhss') && strcmp(jammingMode, 'narrowband')
+                        freq = freqs(1:jammingPara);
+                        power = powers;
+
+                    else
+                        freq = freqs;
+                        power = powers(jammingPara);
+                    end    
+                    for f = freq
+                    	jammer.jam(f, power);
+                    end
+
+                    receivedData(:,testRepetition+1) = receiver.receive();
+                    medium.clear();
+                    testRepetition = testRepetition+1;
+                end
+                %get the bit error rate fore each sample and average them
+                bitErrors = sum(receivedData ~= sentData,1);
+
+                meanBitError = mean(bitErrors);
+
+                % test result contains: chippingRate,chipLength,freqs, powers, bit errors
+                testResults(:,configRan+1) = [ chippingRate, chipLength, jammingPara, power, totalSenders, meanBitError];
+
+                configRan = configRan+1;
             end
-
-            %save data to compare with received data
-            sentData(:,testIter) = randomNumbers(:,subTestIter);
-
-            %let all senders send  
-            for sender = senders
-                sender.send(randomNumbers(:,subTestIter));
-                subTestIter = subTestIter+1;
-            end
-
-            %jam on differend frequencies
-            if strcmp(mode,'fhss') && strcmp(jammingMode, 'narrowband')
-                jammer.jam(freqs(jammingPara), powers);
-            else
-                jammer.jam(freqs, powers(jammingPara));
-            end    
-             
-            receivedData(:,testIter) = receiver.receive();
-            medium.clear();
+            
         end
-      end    
-   end
+       
+	end
 end
 %% output
-i=0;
+% testResults
+figure(1000);
+count = 1;
+for i = 1:length(chippingRates)
+	for j = 1:length(chipLengths)
+        start = 1+(count-1)*jammingParaLength*numberOfSenders;
+        ende = start+jammingParaLength*numberOfSenders-1;
+        %either there are multiple users or multiple power levels
+        X = testResults(4,start:ende).*testResults(5,start:ende).*testResults(3,start:ende);
+        Y = testResults(6,start:ende);
+        scatter(X,Y);
+        hold on
+       
+        legendEntries{count} = ['Chipping rate:', num2str(chippingRates(i)), ', Chip Length:', num2str(chipLengths(j)), ' , number of senders:', num2str(numberOfSenders)];
+         
+        count = count+1;
+    end
+end    
+legend(legendEntries);
+hold off
+
