25ed231151244dc39a3aaf69a4344472e0367ec1
gianlucamateo <gianluca.mateo@gmail.com>
2014-12-05 17:21:00 +0100

    fhss tests now use 3*chiplength to account for channel word creation

--- matlab_code/jamming_tests.m
+++ matlab_code/jamming_tests.m
@@ -7,24 +7,29 @@ Fs = 4096;
 
 medium = Medium;
 
-dataRate = 4;
-chippingRate = 64;
+dataRate = 8;
+chippingRate = 8;
 
-pnGenerator = PNGenerator(3*12);
+pnGenerator = PNGenerator(16*3);
 sequence = pnGenerator.step();
-sender = Sender(medium, sequence, 'dsss', Fs, dataRate,chippingRate);
+sender = Sender(medium, sequence, 'fhss', Fs, dataRate,chippingRate);
 sender.setGaussianSNR(10);
 
 jammer = Jammer(medium, Fs);
 
-test_data = randi([0,1],100,1);
+test_data = randi([0,1],1024,1);
 
 sender.send(test_data);
 
-jammer.jam(100, 16 , 2.5); %frequency, bandwidth (power of 2 optimally), power in multiples of standard output
+jammer.jam(5, 5 , 3); %frequency, bandwidth (power of 2 optimally), power in multiples of standard output
+%jammer.jam(150, 50 , 3);
+%jammer.jam(200, 50 , 2);
+%jammer.jam(250, 50 , 2);
+%jammer.jam(300, 50 , 2);
+%jammer.jam(350, 50 , 2);
+%jammer.jam(400, 50 , 2);
 
-
-receiver = Receiver(medium, sequence, 'dsss', Fs, dataRate, chippingRate);
+receiver = Receiver(medium, sequence, 'fhss', Fs, dataRate, chippingRate);
 
 received = receiver.receive();
 
